c7af9b9d71c49617445a5606ad77bc9547024e65
pyd6802 <55672489+pyd6802@users.noreply.github.com>
2019-10-28 00:00:48 -0400

    Update index.php

--- index.php
+++ index.php
@@ -47,25 +47,23 @@
     echo 'Connected successfully';
 
 // Performing SQL query
-//$sql = "SELECT * FROM Location;"
-//if (!$result = $mysqli->query($sql)) {
+$sql = "SELECT * FROM Location;"
+if (!$result = $mysqli->query($sql)) {
     // Oh no! The query failed. 
-  //  echo "Sorry, the website is experiencing problems.";
+  echo "Sorry, the website is experiencing problems.";
 
-    // Again, do not do this on a public site, but we'll show you how
-    // to get the error information
-    //echo "Error: Our query failed to execute and here is why: \n";
-    //echo "Query: " . $sql . "\n";
-    //echo "Errno: " . $mysqli->errno . "\n";
-    //echo "Error: " . $mysqli->error . "\n";
-    //exit;
-//}
+  echo "Error: Our query failed to execute and here is why: \n";
+  echo "Query: " . $sql . "\n";
+  echo "Errno: " . $mysqli->errno . "\n";
+  echo "Error: " . $mysqli->error . "\n";
+  exit;
+}
 
 //if ($result->num_rows === 0) {
   //  echo "Error - Table empty";
     //exit;}
        
-//echo "Number of rows" . $result->num_rows;
+echo "Number of rows" . $result->num_rows;
 
 
 // Printing results in HTML
