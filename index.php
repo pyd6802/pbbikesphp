f3b0bdf1b1c80ba2046f8572ac3f2359bb613952
pyd6802 <55672489+pyd6802@users.noreply.github.com>
2019-10-31 17:04:07 -0400

    Update index.php

--- index.php
+++ index.php
@@ -31,6 +31,39 @@
  
  <h1>Show me the data!</h1>
 
+<html>
+
+    <head>
+
+        <title> PHP UPDATE DATA </title>
+
+        <meta charset="UTF-8">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    </head>
+
+    <body>
+
+        <form action="php_update_data_from_mysql_database_table.php" method="post">
+
+            ID To Update: <input type="text" name="id" required><br><br>
+
+            New First Name:<input type="text" name="fname" required><br><br>
+
+            New Last Name:<input type="text" name="lname" required><br><br>
+
+            New Age:<input type="number" name="age" required><br><br>
+
+            <input type="submit" name="update" value="Update Data">
+
+        </form>
+
+    </body>
+
+
+</html>    
+    
 <?php
 // Connecting, selecting database
     echo 'Starting Connection Process';
