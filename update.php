2b5cf64ce40502da90af76bd6fffc445cce4bd8d
pyd6802 <55672489+pyd6802@users.noreply.github.com>
2019-11-05 21:19:05 -0500

    Update update.php

--- update.php
+++ update.php
@@ -1,4 +1,53 @@
-<!DOCTYPE html>
+?php
+
+// php code to Update data from mysql database Table
+
+if(isset($_POST['update']))
+
+{
+ $link = mysqli_connect('34.68.111.23', 'root', 'pbbikesmysql', 'pbbikes');
+// Check connection
+
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+       
+   // get values form input text and number
+   
+   $Location_id = $_POST['id'];
+   $Capacity = $_POST['capacity'];
+   
+ 
+   $query = "Select Bike_Capacity from Location WHERE LocID = $Location_id;";
+   $result = mysqli_query($link, $query);
+	
+   if ($row = $result->fetch_row()) 
+	   {	   
+       if ($row[0]) < $Capacity {
+	       echo 'Estimate must be less than rack Capacity'; } 
+	   else { mysqli_free_result($result);
+              $query = "UPDATE Location SET Estimated_Space=$Capacity WHERE LocID = $Location_id;";
+              $result = mysqli_query($link, $query);
+              if($result){
+		         echo 'Data Updated';}
+              else{
+                 echo 'Invalid Location ID';
+	             echo "Error: Our query failed to execute and here is why not: \n";
+	             echo "Query: " . $query . "\n";
+	             echo "Errno: " . $mysqli->errno . "\n";
+	             echo "Error: " . $mysqli->error . "\n";
+                  }
+             }
+
+        }
+
+ mysqli_free_result($result);
+ mysqli_close($link);
+}	
+
+?>
+
+	       <!DOCTYPE html>
 
 <html>
     <head>
