fd7beced11691bb82a9ed0639951ac5c8511602a
pyd6802 <55672489+pyd6802@users.noreply.github.com>
2019-11-22 23:21:03 -0500

    Update update.php

--- update.php
+++ update.php
@@ -5,6 +5,7 @@
 if(isset($_POST['update']))
 
 {
+// $link = mysqli_connect('34.68.111.23', 'root', 'pbbikesmysql', 'pbbikes');
  $link = mysqli_connect('pbbikesaws.cl1i7i33btse.us-east-1.rds.amazonaws.com', 'admin', 'pbbikesaws', 'pbbikes');
 // Check connection
 
@@ -12,27 +13,24 @@ if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
        
-   // get values form input text and number
-   
-   $Location_id = $_POST['id'];
-   $Capacity = $_POST['capacity'];
+    
+   $Message = ' ';
+   $Location_id = $_POST['ID'];
+   $Capacity = $_POST['Capacity'];
            
    // mysql query to Update data
-   //"UPDATE persons SET email='peterparker_new@mail.com' WHERE id=1"
+    
+   $query = "UPDATE Location SET Estimated_Space=$Capacity WHERE LocID = $Location_id;";
    
-   $query = "UPDATE Location SET Bike_Capacity=$Capacity WHERE LocID = $Location_id;";
    $result = mysqli_query($link, $query);
-   
-   if($result)
-   {
-       echo 'Data Updated';
-   }else{
-       echo 'Invalid Location ID';
-	   echo "Error: Our query failed to execute and here is why: \n";
-	   echo "Query: " . $query . "\n";
-	   echo "Errno: " . $mysqli->errno . "\n";
-	   echo "Error: " . $mysqli->error . "\n";
-   }
+ 
+   if(mysqli_affected_rows($link)>0)
+   {      
+ 	$Message = 'was updated';} 
+   else{
+        $Message = 'could not be updated';}
+
+//   $result-->free();
    mysqli_close($link);
 }
 
@@ -50,20 +48,25 @@ if($link === false){
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
+
     <body>
+
     <iframe width="620" height="340" 
      src="https://datastudio.google.com/embed/reporting/85de1e89-2f67-489a-98f1-7db3fc3c5d5a/page/BoX4" 
      frameborder="0" style="border:0" allowfullscreen></iframe>
+
         <form action="update.php" method="post">
-            Location ID To Update: <input type="number" name="id" required><br><br>
-            Estimated Capacity:    <input type="number" name="capacity" required><br><br>
+            Location ID To Update: <input type="number" name="ID" required><br><br>
+            Estimated Capacity:    <input type="number" name="Capacity" required><br><br>
             <input type="submit" name="update" value="Update Data">
         </form>
-	<p id="status-message">Estimated Capacity data could not be updated 
+
+	<p id="status-message">Estimated Capacity data <?php echo $Message ?> 
         
         <script>
            setTimeout(function(){
             document.getElementById('status-message').style.display = 'none'; }, 10000);
          </script>
+
     </<body>
 </html>
