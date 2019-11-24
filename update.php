0dc6b3fcb79eaddebdfa140fa73b5f9adb084db1
pyd6802 <55672489+pyd6802@users.noreply.github.com>
2019-11-24 14:54:50 -0500

    Update update.php

--- update.php
+++ update.php
@@ -37,36 +37,55 @@ if($link === false){
 ?>
 
 <!DOCTYPE html>
-
 <html>
-    <head>
-        <title> Capacity Crowd Sourcing </title>
-	<h1> Pittsburgh Bikes Crowd Sourcing </h1>
-        <hr>
-        <h2> Use this form to provide your capacity estimate </h2>  
-        <hr>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    </head>
+	
+<font face="verdana" color="orange"> 
 
+  
+  <head>
+    <meta charset="UTF-8">
+    <title style="text-align: center;">Help, I need to park my bike in Pittsburgh!</title>
+  </head>
+
+  <body style="background-color:black;">
+ 
+    <h1 style="text-align: center;"> <b> Pittsburgh Bikes Capacity Crowd Sourcing</b> </h1>
+	  
+    <h2 style="text-align: center;">
+    Update the Estimated Capacity and Help Your Fellow Cyclist Find Parking<g=/h2>
+    
+    <h3 style="text-align: center;">Enter the Bike Location ID and Update the Estimated Capacity</h3>
+    
+ 
     <body>
-
-    <iframe width="620" height="340" 
-     src="https://datastudio.google.com/embed/reporting/85de1e89-2f67-489a-98f1-7db3fc3c5d5a/page/BoX4" 
-     frameborder="0" style="border:0" allowfullscreen></iframe>
-
+    <div align="center">
+    
+       <iframe width="620" height="340" 
+       src="https://datastudio.google.com/embed/reporting/85de1e89-2f67-489a-98f1-7db3fc3c5d5a/page/BoX4" 
+       frameborder="0" style="border:0" allowfullscreen></iframe>
+ 
+    </div>
+	    
+	    <p>  </p>
+	    
+    <div align="center">    
         <form action="update.php" method="post">
             Location ID To Update: <input type="number" name="ID" required><br><br>
             Estimated Capacity:    <input type="number" name="Capacity" required><br><br>
             <input type="submit" name="update" value="Update Data">
         </form>
+     </div>
 
-	<p id="status-message">Estimated Capacity data <?php echo $Message ?> 
+     <p id="status-message">Estimated Capacity data <?php echo $Message ?> 
         
         <script>
            setTimeout(function(){
             document.getElementById('status-message').style.display = 'none'; }, 10000);
          </script>
 
+     <div align ="center">
+        <button onclick="window.location.href='http://mypittsburghbikes.com/index.php';
+	">   Click Here To See Location Details and Steet View of Racks  </button>
+     </div>
     </<body>
 </html>
