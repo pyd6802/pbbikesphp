0b0c3fd53d73c4e9523c7b3de3f15a1c7c230641
pyd6802 <55672489+pyd6802@users.noreply.github.com>
2019-11-24 13:01:04 -0500

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     <p style="text-align: center;">Click on the tree map location to see where the rack is located</p>
     
     <div align="center">
-	  <iframe width="800" height="490" 
+	  <iframe width="810" height="495" 
 	  src="https://app.powerbi.com/view?r=eyJrIjoiYzlmNDVjZDYtMmQzOS00NTQ0LWJhM2QtZWQxMzI5OGUwZWE0IiwidCI6IjRjMjViOGE2LTE3ZjctNDZmOS04M2YwLTU0NzM0YWI4MWZiMSIsImMiOjN9" 
 	  frameborder="0" allowFullScreen="true"></iframe>
 	  </div>
